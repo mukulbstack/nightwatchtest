4516f7e7c93d8b499526bfa5dbb5b8c861d89787
Mukul Gupta <mukul@browserstack.com>
2016-11-15 19:37:53 +0530

    relative paths cont.

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,4 +1,4 @@
 module.exports = (function(settings) {
   settings.test_workers = false;
   return settings;
-})(require('/Users/mukulg/Documents/code/testing/nightwatch/nightwatch.json'));
+})(require('./nightwatch.json'));
